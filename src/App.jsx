be02d98d03a3f2d78d0511fd3278e4c5859b1132
ThiagoThe <nitrox2thi@gmail.com>
2023-03-12 11:42:06 -0300

    atualizado app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Card from "./components/Card";
 function App() {
   const usuario = {
     nome: "Thiago Pires",
-    idade: 24,
+    idade: 33,
     foto: "https://avatars.githubusercontent.com/u/34431813?v=4",
     github: "https://github.com/ThiagoThe",
     linkedin: "https://www.linkedin.com/in/thiago-pereira-pires-293a61158/",

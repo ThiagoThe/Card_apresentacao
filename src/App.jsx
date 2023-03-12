2a886e236c12795c46b88be01fa413c2fdc0f7d6
ThiagoThe <nitrox2thi@gmail.com>
2023-03-12 11:19:46 -0300

    atualizado app e card

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const usuario = {
+    nome: "Thiago Pires",
+    idade: 24,
+    foto: "https://avatars.githubusercontent.com/u/34431813?v=4",
+    github: "https://github.com/ThiagoThe",
+    linkedin: "https://www.linkedin.com/in/thiago-pereira-pires-293a61158/",
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Card usuario={usuario} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,26 @@ function Card({ usuario }) {
     <div className="card-dados">
       <h1>{usuario.nome}</h1>
       <h2>Idade: {usuario.idade} anos</h2>
-      <div className="card-contatos"></div>
+      <div className="card-contatos">
+        <a href="{usuario.gtihub}" target="_blank">
+          <box-icon
+            type="logo"
+            name="github"
+            color="white"
+            size="md"
+            title="Link para o GitHub"
+          ></box-icon>
+        </a>
+        <a href="{usuario.linkedin}" target="_blank">
+          <box-icon
+            type="logo"
+            name="linkedin"
+            color="white"
+            size="md"
+            title="Link para o LinkedIn"
+          ></box-icon>
+        </a>
+      </div>
     </div>
   </div>;
 }
